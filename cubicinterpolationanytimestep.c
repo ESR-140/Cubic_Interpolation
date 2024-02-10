161ebab6be1b74c25a87327b6fc55c4c6c9696e3
ESR-140 <97493142+ESR-140@users.noreply.github.com>
2024-02-10 23:58:32 +0530

    Update cubicinterpolationanytimestep.c

--- cubicinterpolationanytimestep.c
+++ cubicinterpolationanytimestep.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-//This code only works for timestep 0.33
 
 typedef struct PointCoords {
     double xval;
     double yval;
 } DataPoint;
 
-void inputCoordinatesManual(DataPoint *points, int n) {
+void inputCoordinatesManual(DataPoint *points, int n) { //This function entails manual input of coords
     for (int i = 0; i < n; ++i) {
         printf("\nEnter coordinates for Point %d:\n", i + 1);
         printf("X Value: ");
@@ -19,7 +18,7 @@ void inputCoordinatesManual(DataPoint *points, int n) {
     }
 }
 
-void inputCoordinatesFromCSV(DataPoint *points, int n, const char *filename) {
+void inputCoordinatesFromCSV(DataPoint *points, int n, const char *filename) { //This function entails input from csv
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         fprintf(stderr, "Error opening file: %s\n", filename);
@@ -36,7 +35,7 @@ void inputCoordinatesFromCSV(DataPoint *points, int n, const char *filename) {
     fclose(file);
 }
 
-void interpolateCubicSpline(int m, double sdy[], DataPoint points[]) {
+void calculate_sdys(int m, double sdy[], DataPoint points[]) { //This function calculates second derivates of y
     int n = m - 2;
 
     double a[n], b[n], c[n], d[n], cdash[n], ddash[n];
@@ -79,60 +78,105 @@ void interpolateCubicSpline(int m, double sdy[], DataPoint points[]) {
     }
 }
 
-void plotCubicSpline(int m, double sdy[], DataPoint points[], DataPoint vels_all[], DataPoint acc_all[], double timestep) {
+void interpolateCubicSpline(int m, double sdy[], DataPoint points[], DataPoint vels_all[], DataPoint acc_all[], double timestep) {
+    // This function will calculate values of y through interpolation and also plot the curve
+
     FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
     if (!gnuplotPipe) {
         fprintf(stderr, "Error opening gnuplot pipe.\n");
         exit(EXIT_FAILURE);
     }
 
-    // Plot the input points
     fprintf(gnuplotPipe, "plot '-' title 'Input Points' with points pointtype 7 pointsize 1.5, '-' with lines title 'Interpolation', '-' title 'Interpolated Points' with points\n");
     for (int i = 0; i < m; i++) {
         fprintf(gnuplotPipe, "%lf %lf\n", points[i].xval, points[i].yval);
     }
     fprintf(gnuplotPipe, "e\n");
 
-    // Generate and plot interpolated points on the curve
     int temp=0;
     for (int i = 0; i < m - 1; i++) {
         for (double t = 0.0; t <= 1.0; t += timestep) {
             // Calculate the x and y coordinates of the interpolated point
-            int A,B,C,D;  // Coefficents of y(j), y(j+1), sdy(j), sdy(j+1)
-            double x = (1 - t) * points[i].xval + t * points[i + 1].xval;
+            double A,B,C,D;  // Coefficents of y(j), y(j+1), sdy(j), sdy(j+1)
+            double x = points[i].xval + t;
+            vels_all[temp].xval = x;
+            acc_all[temp].xval = x;
 
             A = (points[i+1].xval - x) / (points[i+1].xval - points[i].xval);
             B = (x - points[i].xval) / (points[i+1].xval - points[i].xval) ;
             C = 0.166667 * (A*A*A - A) * (points[i+1].xval - points[i].xval)* (points[i+1].xval - points[i].xval);
             D = 0.166667 * (B*B*B - B) * (points[i+1].xval - points[i].xval) * (points[i+1].xval - points[i].xval);
 
-            // double y = A * points[i].yval + B * points[i+1].yval + C * sdy[i] + D * sdy[i+1];
-
-            double y = sdy[i] * (points[i + 1].xval - x) * (points[i + 1].xval - x) * (points[i + 1].xval - x) / 6.0 +
-                       sdy[i + 1] * (x - points[i].xval) * (x - points[i].xval) * (x - points[i].xval) / 6.0 +
-                       (points[i].yval / (points[i + 1].xval - points[i].xval) -
-                        sdy[i] * (points[i + 1].xval - points[i].xval) / 6.0) *
-                           (points[i + 1].xval - x) +
-                       (points[i + 1].yval / (points[i + 1].xval - points[i].xval) -
-                        sdy[i + 1] * (points[i + 1].xval - points[i].xval) / 6.0) *
-                           (x - points[i].xval);
-            
-            vels_all[temp].xval = x;
+            double y = A * points[i].yval + B * points[i+1].yval + C * sdy[i] + D * sdy[i+1];
             vels_all[temp].yval = y;
-            acc_all[temp].xval = x;
+            
             if (temp != 0) {
-                acc_all[temp].yval = (vels_all[temp].yval - vels_all[temp-1].xval) / timestep;   // acc is first der of vel. Therefore (x2-x1)/(y2-y1)
+                acc_all[temp].yval = (vels_all[temp].yval - vels_all[temp-1].yval) / (vels_all[temp].xval - vels_all[temp-1].xval);   // acc is first der of vel. Therefore 
             }
             else {acc_all[temp].yval = 0;}
-            temp += 1;
-            
             fprintf(gnuplotPipe, "%lf %lf\n", x, y);
+            temp += 1;
         }
     }
     fprintf(gnuplotPipe, "e\n");
 
     for (int i=0; i<(m-1)*((1/timestep) +1);i++){
         fprintf(gnuplotPipe, "%lf %lf\n", vels_all[i].xval, vels_all[i].yval);
+    }
+
+    fclose(gnuplotPipe);
+}
+
+double lagrange_term(int n,DataPoint points[], double x_val) {
+    double result = 0.0;
+    for (int i = 0; i < n; i++) {
+        double term = points[i].yval;
+        for (int j = 0; j < n; j++) {
+            if (j != i) {
+                term *= (x_val - points[j].xval) / (points[i].xval - points[j].xval);
+            }
+        }
+        result += term;
+    }
+    return result;
+}
+
+void interpolateLagrange(int m, DataPoint points[], DataPoint vels_all[], DataPoint acc_all[], double timestep){
+
+    FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+    if (!gnuplotPipe) {
+        fprintf(stderr, "Error opening gnuplot pipe.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(gnuplotPipe, "plot '-' title 'Input Points' with points pointtype 7 pointsize 1.5, '-' with lines title 'Interpolation', '-' title 'Interpolated Points' with points\n");
+    for (int i = 0; i < m; i++) {
+        fprintf(gnuplotPipe, "%lf %lf\n", points[i].xval, points[i].yval);
+    }
+    fprintf(gnuplotPipe, "e\n");
+
+
+    int temp = 0;
+    for (int i = 0; i < m - 1; i++) {
+        for (double t = 0.0; t <= 1.0; t += timestep){
+            double x = points[i].xval + t;
+            double y = lagrange_term(m, points, x);
+
+            vels_all[temp].xval = x;
+            acc_all[temp].xval = x;
+            vels_all[temp].yval = y;
+            if (temp != 0) {
+                acc_all[temp].yval = (vels_all[temp].yval - vels_all[temp-1].yval) / (vels_all[temp].xval - vels_all[temp-1].xval) ;   // acc is first der of vel. Therefore (x2-x1)/(y2-y1)
+            }
+            else {acc_all[temp].yval = 0;}
+            fprintf(gnuplotPipe, "%lf %lf\n", x, acc_all[temp].yval);
+            temp += 1;
+       }
+    }   
+    fprintf(gnuplotPipe, "e\n");
+
+    for (int i=0; i<(m-1)*((1/timestep) +1);i++){
+       fprintf(gnuplotPipe, "%lf %lf\n", vels_all[i].xval, vels_all[i].yval);
     }
 
     fclose(gnuplotPipe);
@@ -159,7 +203,6 @@ void inputCoordinates(int n, DataPoint *points) {
     } while (choice != 1 && choice != 2);
 }
 
-
 int main() {
     int m;
     double timestep;
@@ -177,10 +220,29 @@ int main() {
     double fdy[m]; // Array of first derivatives at each point
 
     inputCoordinates(m, points);
-    interpolateCubicSpline(m, sdy_nodes, points);
-    plotCubicSpline(m, sdy_nodes, points, vels_all, acc_all,timestep);
+
+    int choice;
+
+    printf("Type 1 for Lagrange Interpolation or Type 2 to for Cubic Interpolation: ");
+    scanf("%d", &choice);
+
+    do {
+        if (choice == 1) {
+            interpolateLagrange(m, points, vels_all, acc_all, timestep);
+        } else if (choice == 2) {
+            calculate_sdys(m, sdy_nodes, points);
+            interpolateCubicSpline(m, sdy_nodes, points, vels_all, acc_all,timestep);
+        } else {
+            printf("Wrong Value entered, Please Try again\n");
+        }
+
+    } while (choice != 1 && choice != 2);
+
+    char filename[100];
+    printf("Enter the path of your output file: ");
+    scanf("%s", filename);
     
-    FILE *csvFile = fopen("output.csv", "w");
+    FILE *csvFile = fopen(filename, "w");
     if (csvFile == NULL) {
         fprintf(stderr, "Error opening file for writing.\n");
         return 1;
@@ -198,9 +260,10 @@ int main() {
                 acc_all[i].yval);
     }
 
-    // Close the CSV file
+    printf("File Saved Successfully!!");
     fclose(csvFile);
 
 
     return 0;
 }
+
